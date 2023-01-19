f2399dc28c3a3a235c3581933d93bb57a22f8b30
inforgstr <xboyxmax@gmail.com>
2023-01-19 23:16:39 +0500

    upd: Modified file

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
 import requests
-from config import TOKEN, API
+from config import API
+import datetime
 
 
 def get_weather(city, open_weather_token):
+
+    code_to_smile = {
+        "Clear": "Clear \U00002600",
+        "Clouds": "Cloudy \U00002601",
+        "Rain": "Rain \U00002614",
+        "Drizzle": "Drizzle \U00002614",
+        "Thunderstorm": "Thunderstorm \U000026A1",
+        "Snow": "Snow \U0001F328",
+        "Mist": "Mist \U0001F32B",
+    }
+
     try:
         req = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
         )
         weather = req.json()
+        description = weather["weather"][0]["main"]
+
+        if description in code_to_smile:
+            wd = code_to_smile[description]
+
+
         city = weather["name"]
-        print(city)
+        temp = f'{weather["main"]["temp"]:.1f}°C'
+        humidity = f'{weather["main"]["humidity"]} %'
+        pressure = weather["main"]["pressure"]
+        feels = f"{weather['main']['feels_like']:.1f}"
+
+
+        code = weather["sys"]["country"]
+        sunrise = datetime.datetime.fromtimestamp(weather["sys"]["sunrise"]).strftime(
+            "%d.%m.%Y  at  %H: %M"
+        )
+        sunset = datetime.datetime.fromtimestamp(weather["sys"]["sunset"]).strftime(
+            "%d.%m.%Y  at  %H: %M"
+        )
+
+        wind = f'{weather["wind"]["speed"]:.1f} m/s'
+
+
+        print(f"******{datetime.datetime.now().strftime('%h %m    %H:%M ')}******\n\n"
+            f"In {city}  temperature  {temp} (feels {feels}) {wd}\n\nHumidity:\t{humidity}\n\nCountry code:\t{code}\n\n"
+            f"Pressure:\t{pressure}\n\nWind:\t\t{wind}\n\nSunrise:\t{sunrise}\n\nSunset:\t\t{sunset}"
+        )
 
     except Exception as ex:
+        print("Error ", ex)
         print("Please, try again!")
 
 
